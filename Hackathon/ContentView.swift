72a61cf5a6a73716652b859d6a5a54e9cedfe125
Bennett Cheung <bennettcheung@gmail.com>
2020-04-28 17:16:55 -0700

    Initial Commit

---
+++ Hackathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hackathon
+//
+//  Created by BC on 2020-04-28.
+//  Copyright © 2020 BC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
